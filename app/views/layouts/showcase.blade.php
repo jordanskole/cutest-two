ade3650f307d8068cb4ae4474c43e0cff2cf7ecb
Jordan Lewis Skole <jordan@robotcouture.com>
2014-03-30 21:53:04 -0400

    showcase views

---
+++ app/views/layouts/showcase.blade.php
@@ -0,0 +1,268 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <title>Showcase - Landing Page</title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width">
+
+  <link rel="stylesheet" href="/showcase/css/bootstrap.min.css">
+  <link rel="stylesheet" href="/showcase/css/helper.css">
+  <link rel="stylesheet" href="/showcase/css/browserfrog.css">
+  <link rel="stylesheet" href="/showcase/css/onepage-scroll.css">
+  <link rel="stylesheet" href="/showcase/css/main.css">
+  <link rel="stylesheet" href="/showcase/css/custom.css">
+
+  <!--[if lte IE 9]>
+  <link rel="stylesheet" type="text/css" href="css/ie9-and-down.css" />
+  <![endif]-->
+  <link href="/showcase/favicon.ico" rel="icon" type="image/x-icon" />
+
+  <script src="/showcase/js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+</head>
+<body>
+
+  <div class="main">
+
+    <section class="home">
+      <div class="container">
+
+        <div class="home-phone hide hidden-xs col-xs-12 col-sm-5">
+          <img src="/showcase/img/iphone-3.png" class="iphone-3 " alt="">
+        </div>
+
+        <div class="row">
+
+          <div class="home-copy  col-xs-12 col-sm-8 col-sm-offset-2  col-md-10 col-md-offset-1 animated fadeInDown delay-2 ">
+            <h1 class="demo1">Wedding websites.<br>As cute as you are.</h1>
+
+            <p>Beautiful wedding websites. Spa-like customization experience.</p>
+
+            <span class="take-a-look animated fadeInDown delay-3"><i class="entypo-down-open"></i>Browse Themes</span>
+          </div><!-- /.home-copy -->
+        </div><!-- /.row -->
+
+      </div><!-- /.container -->
+    </section>
+
+    <section class="slide-1 slide full-color">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-sm-4 col-xs-12 copy">
+            <div class="browser outline">
+              <div class="bar">
+                <div class="home"></div>
+                <div class="addressbar">
+                  <div class="search"></div>
+                </div><!-- /.addressbar -->
+                <div class="settings"></div>
+              </div><!-- /.bar -->
+              <div class="content">
+                <img src="/showcase/img/eternity.png">
+              </div><!-- /.content -->
+            </div><!-- /.browser -->
+            <h2 class="text-center slide-titles">Eternity</h2>
+            <p class="">
+              Eternity is a beautiful theme featuring a left-hand menu.
+            </p>
+            <p class="">
+              <a class="btn take-a-look" href="javascript:;" data-toggle="modal" data-target="#eternity-modal">Get Started <i class="entypo-right-open"></i></a>
+            </p>
+          </div><!-- /.col-sm-4 -->
+
+          <div class="col-sm-4 col-xs-12 copy">
+            <div class="browser outline">
+              <div class="bar">
+                <div class="home"></div>
+                <div class="addressbar">
+                  <div class="search"></div>
+                </div><!-- /.addressbar -->
+                <div class="settings"></div>
+              </div><!-- /.bar -->
+              <div class="content">
+                <img src="/showcase/img/eternity.png">
+              </div><!-- /.content -->
+            </div><!-- /.browser -->
+            <h2 class="text-center slide-titles">Garis</h2>
+            <p class="">
+              Garis the perfect theme for showing off your engagement photos.
+            </p>
+            <p class="">
+              <a class="btn btn-warning take-a-look" href="themes/eternity">Coming soon <i class="entypo-right-open"></i></a>
+            </p>
+          </div><!-- /.col-sm-4 -->          
+
+          <div class="col-sm-4 col-xs-12 copy">
+            <div class="browser outline">
+              <div class="bar">
+                <div class="home"></div>
+                <div class="addressbar">
+                  <div class="search"></div>
+                </div><!-- /.addressbar -->
+                <div class="settings"></div>
+              </div><!-- /.bar -->
+              <div class="content">
+                <img src="/showcase/img/eternity.png">
+              </div><!-- /.content -->
+            </div><!-- /.browser -->
+            <h2 class="text-center slide-titles">Twilli</h2>
+            <p class="">
+              Twilli is our newest theme. You'll love it!
+            </p>
+            <p class="">
+              <a class="btn btn-warning take-a-look" href="themes/eternity">Coming soon <i class="entypo-right-open"></i></a>
+            </p>
+          </div><!-- /.col-sm-4 -->                    
+
+        </div><!-- /.row -->
+      </div><!-- /.container -->
+    </section><!-- /.slide-1 -->
+
+    <section class="slide-2 slide full-color">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-xs-12 col-sm-4 copy">
+            <h2 class=" hide slide-2-title slide-titles">  Even a MOB could use it.  </h2>
+
+            <p class="slide-2-1 hide">
+              We
+            </p>
+
+            <p class="slide-2-2 hide hidden-xs">
+              Simply slide between slides just likes Apple's latest series of promotion sites.
+            </p>
+
+            <p class="slide-2-3 hide hidden-xs">
+              Remember simplicty will keep your work looking its best. Keep it simple.
+            </p>
+          </div>
+
+          <div class="col-xs-12 col-sm-8 iphone-container">
+            <img src="/showcase/img/macbook.png" class="hide iphone-2" alt="">
+          </div>
+
+        </div><!-- /.row -->
+      </div><!-- /.container -->
+    </section><!-- /.slide-2 -->
+
+    <section class="slide-3 slide full-color">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12 col-sm-4 copy">
+            <h2 class=" hide slide-3-title slide-titles">  Obvisouly its Responsive </h2>
+            <p class="slide-3-1 hide">
+              Works great all all screen sizes. Use bootstraps default classes hidden-xs hidden-sm to control what you see.
+            </p>
+
+            <p class="slide-3-2 hide hidden-xs">
+              Show only the top half of the phone or hide it all together on mobile.
+            </p>
+
+            <p class="slide-3-3 hide hidden-xs">
+              PSD files are added for your screenshots, drop them right in and be up and running quickly.
+            </p>
+            <i class="hide entypo-down-open slide-3-scroll scroll-down hidden-xs"></i>
+          </div>
+
+          <div class="col-xs-12 col-sm-6 iphone-container ">
+            <img src="/showcase/img/iphone-3.png" class="hide iphone-3" alt="">
+          </div>
+        </div>
+
+      </div><!-- /.container -->
+    </section><!-- /.slide-3 -->
+
+    <section class="contact full-color">
+      <div class="row">
+        <div class="col-xs-12 col-sm-6 col-sm-offset-3 col-md-6 col-md-offset-3">
+          <h2>Keep in touch</h2>
+          <p> We're always keen to here what you think, send us a message from all the usual places. Don't forget to sign up for our newsletter.</p>
+
+          <div class="newsletter clearfix">
+            <form role="form">
+              <div class="form-group">
+                <input type="text" id="emailSignup" name="signup" placeholder="Your Email">
+                <button type="submit" class="btn btn-signup take-a-look"><i class="entypo-mail"></i></button>
+              </div>
+            </form>
+          </div>
+
+          <div class="social clearfix">
+            <a href="#" class="social-item facebook"><i class="entypo-facebook"></i></a>
+            <a href="#" class="social-item twitter"><i class="entypo-twitter"></i></a>
+          </div>
+        </div>
+      </div><!-- /.row -->
+    </section><!-- /.contact -->
+
+  </div> <!-- /main --> 
+
+  <!-- location agnostic features -->
+  <!-- logo -->
+  <div class="icon animated fadeInLeft delay-3">
+    <span> The Cutest Two </span>
+  </div>
+  <!-- our modals -->
+  <div id="eternity-modal" class="modal fade" role="dialog" aria-labelledby="eternity-modal" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+          <h4 class="modal-title text-info">Let's Get Started</h4>
+        </div><!-- /.modal-header -->
+        <div class="modal-body">
+          {{ Form::open(['route'=>'tour'])}}
+          <div class="row mtop0">
+            <div class="col-sm-6 form-group">
+              <label class="text-muted" for="name_1">Bride:</label>
+              <input type="text" class="form-control input-lg" name="name_1" placeholder="Heather">
+            </div><!-- /.col-sm-6 -->
+            <div class="col-sm-6 form-group">
+              <label class="text-muted" for="name_2">Groom:</label>
+              <input type="text" class="form-control input-lg" name="name_2" placeholder="Jordan">
+            </div><!-- /.col-sm-6 -->
+          </div><!-- /.row -->
+          <div class="row mtop0">
+            <div class="col-sm-4 form-group">
+              <label class="text-muted" for="date">The Big Day:</label>
+              <input type="date" class="form-control" name="date" id="date" placeholder="Aug 16">
+            </div><!-- /.col-sm-5 -->
+            <div class="col-sm-8">
+              <label class="text-muted" for="date">The Venue:</label>
+              <input type="text" class="form-control" name="venue" placeholder="The Grand Ballroom">
+            </div><!-- /.col-sm-5 -->
+          </div><!-- /.row -->
+          <div class="row">
+            <div class="col-sm-4 form-group">
+              <label class="text-muted" for="primary_color">Color:</label>
+              <select class="form-control">
+                <option>Purpleish</option>
+              </select>
+            </div><!-- /.col-sm-6 -->
+          </div><!-- /.row -->
+        </div><!-- /.modal-body -->
+        <div class="modal-footer">
+          <button type="submit" class="btn btn-default pull-right take-a-look">Show me!</button>
+          {{ Form::close() }}
+        </div><!-- /.modal-footer -->
+      </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+  </div><!-- /.modal -->
+
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+
+  <script>window.jQuery || document.write('<script src="/showcase/js/vendor/jquery-1.10.1.min.js"><\/script>')</script>
+
+  <script src="/showcase/js/vendor/bootstrap.min.js"></script>
+  <script src="/showcase/js/vendor/jquery-ui-1.8.22.custom.min.js"></script>
+  <script src="/showcase/js/vendor/jquery.imagesloaded.min.js"></script>
+  <script src="http://vjs.zencdn.net/4.0/video.js"></script>
+  <script src="/showcase/js/vendor/bigvideo.js"></script>
+  <script src="/showcase/js/vendor/jquery.simple-text-rotator.min.js"></script>
+  <script src="/showcase/js/vendor/jquery.onepage-scroll.min.js"></script>
+  <script src="/showcase/js/main.js"></script>
+</body>
+</html>
